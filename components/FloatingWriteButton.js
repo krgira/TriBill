bb03c7b7c28bc770771d6d718b2320a476e22a15
krgira <12181572@inha.edu>
2023-05-26 16:02:21 +0900

    0526 krgira button apply

--- components/FloatingWriteButton.js
+++ components/FloatingWriteButton.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Platform, Pressable, StyleSheet, View } from "react-native";
-import { Ionicons  } from '@expo/vector-icons';
+import { StyleSheet, View, Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 function FloatingWriteButton() {
@@ -11,46 +10,20 @@ function FloatingWriteButton() {
 
     return (
         <View style={styles.wrapper}>
-            <Pressable style={({pressed}) => [
-                styles.button,
-                Platform.OS === 'ios' && {
-                    opacity: pressed ? 0.6 : 1,
-                },
-            ]}
-            android_ripple={{color: 'white'}}
-            onpress={onPress}>
-            </Pressable>
+            <Button 
+            title="새 가계부 등록하기"
+            //style={styles.button}
+            onPress={onPress} />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     wrapper: {
-        position: 'absolute',
-        bottom: 16,
-        right:16,
-        width: 56,
-        height: 56,
-        borderRadius: 28,
-        // ios 전용 그림자
-        shadowColor: '#4d4d4d',
-        shadowOffset: {width:0, height:4},
-        shadowOpacity: 0.3,
-        shadowRadius: 4,
-        // android 전용 그림자
-        elevation: 5,
-        overflow: Platform.select({android: 'hidden'}),
+        backgroundColor: "#4974A5",
     },
     button: {
-        width: 56,
-        height: 56,
-        borderRadius: 28,
-        backgroundColor: '#009688',
-        justifyContent: 'center',
-        alignContent: 'center',
-    },
-    icon: {
-        color: 'white',
+
     },
 });
 
