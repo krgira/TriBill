4e6a72463c6fc83af7a14a4db2c3c73458031a3d
최국희 <choigukhee@MacBookcookie.local>
2023-05-31 11:21:50 +0900

    충돌

--- App.js
+++ App.js
@@ -4,23 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import RootStack from './screens/RootStack';
 import { LogContextProvider } from './contexts/LogContext';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDP8UMn1BEfAsdXqAMrDNJtqhUYKAOWncg",
-  authDomain: "tribill-eaf40.firebaseapp.com",
-  projectId: "tribill-eaf40",
-  storageBucket: "tribill-eaf40.appspot.com",
-  messagingSenderId: "586768083106",
-  appId: "1:586768083106:web:78e28d40cb39c6dae60ffb",
-  measurementId: "G-LJ0DZ4F46R"
-};
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
 
 const App = () => {
   return (
