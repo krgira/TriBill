61ce6f64a9a22f98b592c4d2248b3d3e3cb4d5e8
최국희 <choigukhee@MacBookcookie.local>
2023-06-10 08:27:59 +0900

    맵

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,9 +1,46 @@
-import React from 'react';
-import { View } from 'react-native';
+import * as React from 'react';
+import { PROVIDER_GOOGLE, Marker  } from 'react-native-maps';
+import MapView from 'react-native-maps';
+import { Button, Image, Text, View, StyleSheet}from 'react-native';
+import axios from 'axios';
 
-function MapScreen() {
-  return <View></View>
-};
+const MapScreen = () => {
+ 
+  return(
+  <View style={styles.screen}>
+  	  <MapView // 셀프클로징해도 되지만 후의 마커를 위해서
+		style={styles.map}
+		initialRegion={{
+            latitude: 37.00000,
+            longitude: 126.00000,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+          }}
+        provider={PROVIDER_GOOGLE}
+		> 
+    <Marker
+            coordinate={{
+            latitude: 37.00000,
+            longitude: 126.00000,
+          }}
+            pinColor="#2D63E2"
+            title="하이"
+            description="테스트"
+          />
+      </MapView>
+  </View>
+  )
+  
+}
 
+export default MapScreen
 
-export default MapScreen;
+const styles = StyleSheet.create({
+	screen:{
+      flex:1
+    },
+  	map:{
+	  width: "100%",
+  	  height : "100%"
+	}
+})
